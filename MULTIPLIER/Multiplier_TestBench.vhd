d626d0a9ed90cee68b8a37e0a44b0fe45c170d12
ABISHEK A T <43531987+abishekat@users.noreply.github.com>
2022-09-22 22:43:18 -0400

    converted 8 to 16 bit multiplier

--- MULTIPLIER/Multiplier_TestBench.vhd
+++ MULTIPLIER/Multiplier_TestBench.vhd
@@ -4,31 +4,31 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
-ENTITY MULTIPLIER_TB IS
-END MULTIPLIER_TB;
+ENTITY MULTIPLIER16_TB IS
+END MULTIPLIER16_TB;
 
-ARCHITECTURE TEST_BENCH OF MULTIPLIER_TB IS
+ARCHITECTURE TEST_BENCH OF MULTIPLIER16_TB IS
 
-COMPONENT multi_8bit
+COMPONENT multi_16bit
 
 PORT(
-	a : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-	x : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-	p : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+	a16 : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+	x16 : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+	p16 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 );
 
 END COMPONENT;
 
 SIGNAL clk, reset : STD_LOGIC := '0';
-SIGNAL multiplier, multiplicand : STD_LOGIC_VECTOR (7 DOWNTO 0);
-SIGNAL product : STD_LOGIC_VECTOR (15 DOWNTO 0);
+SIGNAL multiplier, multiplicand : STD_LOGIC_VECTOR (15 DOWNTO 0);
+SIGNAL product : STD_LOGIC_VECTOR (31 DOWNTO 0);
 
 CONSTANT clk_period : time := 10ns;
 
 BEGIN
-	uut: multi_8bit
+	uut: multi_16bit
 	PORT MAP(
-		a => multiplier, x => multiplicand, p => product
+		a16 => multiplier, x16 => multiplicand, p16 => product
 	);
 	
 	
@@ -43,8 +43,8 @@ BEGIN
 	
 	stimulus : PROCESS
 	BEGIN
-		multiplier <= "00001000";
-		multiplicand <= "00000010";
+		multiplier <= "0000000000000001";
+		multiplicand <= "0000000000000010";
 	WAIT;
 	END PROCESS;
 
